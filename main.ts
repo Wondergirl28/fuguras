b341de6cea0ff23521bd4c651c7622c480ab9d37
Wondergirl28 <88722170+Wondergirl28@users.noreply.github.com>
2021-08-27 09:01:00 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,13 @@
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+    led.plot(2, 1)
+    led.plot(3, 2)
+    led.plot(4, 1)
+    led.plot(2, 3)
+    led.plot(1, 0)
+    led.plot(0, 1)
+    led.plot(1, 2)
+    led.plot(3, 0)
+})
